a05ce40f5943b5e11eb4a88b70eda51e6c021bdd
NaHyunNam <namnahyundev@gmail.com>
2020-01-28 20:03:24 +0900

    oop_study_five_ inheritance1_주석추가

--- inheritance.php
+++ inheritance.php
@@ -24,7 +24,7 @@ class Human extends Animal{
         print('talking...<br>');
     }
 }
-$human = new Human();
-$human->run();
-$human->think();
+$human = new Human(); // Human 인스턴스
+$human->run(); // Animal 클래스에서 정의한 함수를 Human 클래스에서 상속 받았기 때문에 사용 가능
+$human->think(); // Human 클래스에서 정의한 함수 사용 가능
 ?>
