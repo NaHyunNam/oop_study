7214177632587c31162206cfd42782423bfa120d
NaHyunNam <namnahyundev@gmail.com>
2020-02-11 18:50:10 +0900

    oop_study_ fourteen_interface3

--- composerApp/index.php
+++ composerApp/index.php
@@ -3,12 +3,19 @@ require_once __DIR__.'/vendor/autoload.php'; // __DIR__는 현재 파일(index.p
 
 use Monolog\Logger;
 use Monolog\Handler\StreamHandler;
+use Monolog\Handler\NativeMailerHandler;
 
 // create a log channel
 $log = new Logger('name');
 $log->pushHandler(new StreamHandler(__DIR__.'/app.log', Logger::WARNING));
+$log->pushHandler(new NativeMailerHandler(
+    'skaskgus0704@naver.com',
+    'Emergence!!!!',
+    'out@system.com',
+    Logger::EMERGENCY));
 
 // add records to the log
 $log->warning('Foo');
 $log->error('Bar');
+$log->emergency('emergency');
 ?>
